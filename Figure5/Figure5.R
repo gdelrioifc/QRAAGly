ffccf9710d2a9a9ccfaafe82ab229dc18ff199e3
gdelrioifc <gdelrioifc@gmail.com>
2024-06-18 16:36:19 -0600

    Add files via upload

---
+++ Figure5/Figure5.R
@@ -0,0 +1,16 @@
+library(scales)
+
+setwd("/Volumes/Disco2024/Papers/EnPreparacion/DProtein/2024/EAAInProteinsAndMetabolism/DataFiguresPaper/Table2")
+
+df <- read.csv('table2.csv')
+
+plot.aminodist <- function(x,labels,color,df,size,alpha) 
+{
+  n <- length(colnames(df))
+  y <- df[,1]
+  plot(x,y,pch=16,cex=size,xaxt='n',xlab="", ylab="RQAA cells/QRAA proteome",col=alpha(color,alpha), ylim=c(0,10))
+  axis(1,at=x,labels=labels,las=2)
+  apply(df[,2:n],MARGIN=2,FUN=function(y) {points(x,y,pch=16,cex=size,col=alpha(color,alpha))})
+}
+plot.aminodist(df[,1],df[,2],c(rep('blue',9),rep('red',11)),df[,3:6],0.75,1.0)
+
