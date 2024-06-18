b2d8b2ba0078b98e43d4d396e0423d747522d282
gdelrioifc <gdelrioifc@gmail.com>
2024-06-18 16:39:28 -0600

    Add files via upload

---
+++ Figure6/Figure6.R
@@ -0,0 +1,16 @@
+library(scales)
+
+setwd("/Volumes/Disco2024/Papers/EnPreparacion/DProtein/2024/EAAInProteinsAndMetabolism/DataFiguresPaper/Figure6")
+
+df <- read.csv('dataFigure6.csv')
+
+plot.aminodist <- function(x,labels,color,df,size,alpha) 
+{
+  n <- length(colnames(df))
+  y <- df[,1]
+  plot(x,y,pch=16,cex=size,xaxt='n',xlab="", ylab="RQAA",col=alpha(color,alpha), ylim=c(0,25))
+  axis(1,at=x,labels=labels,las=2)
+  apply(df[,2:n],MARGIN=2,FUN=function(y) {points(x,y,pch=16,cex=size,col=alpha(color,alpha))})
+}
+plot.aminodist(df[,1],df[,2],c(rep('blue',9),rep('red',11)),df[,3:4908],0.75,0.01)
+
